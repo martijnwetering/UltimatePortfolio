bfd2fc2fdc81492e17d1c430ecfa76aa7da7808c
Martijn van de Wetering <mvandewetering@easysystems.nl>
2022-05-17 21:06:03 +0200

    Initial Commit

---
+++ UltimatePortfolio/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UltimatePortfolio
+//
+//  Created by Martijn van de Wetering on 17/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Martijn van de Wetering on 17/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
